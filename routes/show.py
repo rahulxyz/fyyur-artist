eac8b20c8b08f9f7f42f1a80507e5f9f5a5d4240
“rahulxyz” <rahulguptaxyz15@gmail.com>
2025-08-28 22:34:51 +0530

    show sql to orm

--- routes/show.py
+++ routes/show.py
@@ -4,13 +4,9 @@ from models import Show
 from flask import render_template, request, flash
 from forms import *
 from models import Venue, Artist, Show
-#  Shows
-#  ----------------------------------------------------------------
 
-@app.route('/shows')
+@app.route("/shows")
 def shows():
-    # displays list of shows at /shows
-    # TODO: replace with real venues data.
     result = (
         db.session.query(
             Venue.id.label("venue_id"),
@@ -18,87 +14,32 @@ def shows():
             Artist.id.label("artist_id"),
             Artist.name.label("artist_name"),
             Artist.image_link.label("artist_image_link"),
-            Show.start_time.label("start_time")
+            Show.start_time.label("start_time"),
         )
         .join(Show, Artist.id == Show.artist_id)
         .join(Venue, Venue.id == Show.venue_id)
-        .all()  
+        .all()
     )
-    sql = """select
-          v.id as "venue_id",
-          v.name as "venue_name",
-          a.id as "artist_id",
-          a.name as "artist_name",
-          a.image_link as "artist_image_link",
-          s.start_time as "start_time"
-        from
-          "Show" s
-        left join "Artist" a on
-          s.artist_id = a.id
-        left join "Venue" v on
-          s.venue_id = v.id"""
-    result = db.session.execute(sql).fetchall()
     data = [
-     {
-        "venue_id": r.venue_id,
-        "venue_name": r.venue_name,
-        "artist_id": r.artist_id,
-        "artist_name": r.artist_name,
-        "artist_image_link": r.artist_image_link,
-        "start_time": r.start_time
-     }
-     for r in result
-  ]
-    # data=[{
-    #   "venue_id": 1,
-    #   "venue_name": "The Musical Hop",
-    #   "artist_id": 4,
-    #   "artist_name": "Guns N Petals",
-    #   "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    #   "start_time": "2019-05-21T21:30:00.000Z"
-    # }, {
-    #   "venue_id": 3,
-    #   "venue_name": "Park Square Live Music & Coffee",
-    #   "artist_id": 5,
-    #   "artist_name": "Matt Quevedo",
-    #   "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    #   "start_time": "2019-06-15T23:00:00.000Z"
-    # }, {
-    #   "venue_id": 3,
-    #   "venue_name": "Park Square Live Music & Coffee",
-    #   "artist_id": 6,
-    #   "artist_name": "The Wild Sax Band",
-    #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #   "start_time": "2035-04-01T20:00:00.000Z"
-    # }, {
-    #   "venue_id": 3,
-    #   "venue_name": "Park Square Live Music & Coffee",
-    #   "artist_id": 6,
-    #   "artist_name": "The Wild Sax Band",
-    #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #   "start_time": "2035-04-08T20:00:00.000Z"
-    # }, {
-    #   "venue_id": 3,
-    #   "venue_name": "Park Square Live Music & Coffee",
-    #   "artist_id": 6,
-    #   "artist_name": "The Wild Sax Band",
-    #   "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    #   "start_time": "2035-04-15T20:00:00.000Z"
-    # }]
-
-    return render_template('pages/shows.html', shows=data)
+        {
+            "venue_id": r.venue_id,
+            "venue_name": r.venue_name,
+            "artist_id": r.artist_id,
+            "artist_name": r.artist_name,
+            "artist_image_link": r.artist_image_link,
+            "start_time": r.start_time,
+        }
+        for r in result
+    ]
+    return render_template("pages/shows.html", shows=data)
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
   form = ShowForm()
   return render_template('forms/new_show.html', form=form)
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
-
   try:
       form = request.form
       show = Show(
@@ -117,9 +58,4 @@ def create_show_submission():
       print(e)
   finally:
       db.session.close()
-  # on successful db insert, flash success
-  
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
   return render_template('pages/home.html')
